80c845120cac1c588d8574d3ae1b108be35238d0
shivansh saxena <150766578+shivansh4565@users.noreply.github.com>
2026-07-09 20:44:36 +0530

    Update Hero.tsx

--- src/Components/Hero.tsx
+++ src/Components/Hero.tsx
@@ -11,11 +11,7 @@ const Hero = () => {
       <div className="relative z-10 max-w-7xl w-full grid lg:grid-cols-2 gap-14 items-center">
         {/* Left */}
         <div>
-          {/* Badge */}
-          <div className="mt-[120px] inline-flex items-center gap-2 rounded-full border border-purple-500/30 bg-purple-500/10 px-5 py-2 text-sm text-purple-300 ">
-            <span className="h-2 w-2 rounded-full bg-green-400"></span>
-            Available for AI/ML & Full Stack Roles
-          </div>
+        
 
           {/* Heading */}
           <h1 className="mt-8 text-5xl md:text-7xl font-bold leading-tight">
